f71474b47381d6652716a7883abdc2bf01248195
bpn07 <beepinpathak0721@gmail.com>
2024-08-21 00:09:22 +0545

    Auth Pages

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import loginImge from "@app/assets/images/login-img.png";
+import { Field, Form, Formik } from "formik";
+import Link from "next/link";
+
+const RegisterPage = () => {
+  return (
+    <div className="px-28 py-10">
+      <p className="text-2xl font-bold">Construction v2</p>
+      <div className="flex items-center justify-between">
+        <div className="w-1/2">
+          <Image src={loginImge} alt="" className="w-full object-cover" />
+        </div>
+        <div className="w-1/3 mr-28">
+          <div>
+            <p className="text-4xl font-black">Welcome!</p>
+            <p className="text-xl font-thin mt-3">
+              Welcome! Please enter your details.
+            </p>
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              onSubmit={() => {}}
+            >
+              <Form className="mt-10 w-full">
+                <Field
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  className="w-full border-b border-[#A69999] py-2.5 outline-none mb-[30px]"
+                />
+                <Field
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  className="w-full border-b border-[#A69999] py-2.5 outline-none mb-[30px]"
+                />
+                <Field
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="Confirm Password"
+                  className="w-full border-b border-[#A69999] py-2.5 outline-none"
+                />
+             
+                <div className="mt-9">
+                  <button
+                    type="submit"
+                    className="text-white rounded-md font-bold bg-black w-full py-5"
+                  >
+                    Sign Up
+                  </button>
+                  <p className="mt-[38px] text-center font-thin">
+                    Already have an account ?{" "}
+                    <span className="font-medium">
+                      <Link
+                        href={"/auth/login"}
+                        className="underline underline-offset-2"
+                      >
+                        Login
+                      </Link>
+                    </span>
+                  </p>
+                </div>
+              </Form>
+            </Formik>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
