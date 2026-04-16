f4e4f8c8ec6fea9bbbb6e0b813a27cf04288cb0c
shiva.senang <shiva@senang.io>
2026-04-16 18:44:46 +0545

    fully functional admin

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': require('path').resolve(__dirname, './src'),
+      '@': require('path').resolve(__dirname, './'),
     };
     return config;
   },
